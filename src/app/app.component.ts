6a746e8dda2361e0edaf308fb37921043af4c9b5
afolabiopakunle <opakunleafolabi@gmail.com>
2022-05-21 06:49:30 +0100

    parked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component, } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, OnInit, } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
   
+  form: FormGroup;
   constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      fullName: [''],
+      email: ['', [Validators.email]],
+      skill: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['']
+      })
+    })
+  }
+
+  loopFormGroup(formGroup: FormGroup) {
+    Object.keys(formGroup.controls).forEach((key: string) => {
+      let abstractControl = formGroup.get(key);
+      if(abstractControl instanceof FormGroup) {
+        this.loopFormGroup(abstractControl)
+      } else {
+        console.log(`Key: ${key} | Value: ${abstractControl.value}`)
+      }
+    })
+  }
+
 }
